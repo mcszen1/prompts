f4d80a292e40e3611dc63c37f8917d2f160f32bc
mcszen1 <mcszen@gmail.com>
2023-11-16 10:43:22 -0300

    Update PromptFormula.py

--- PromptFormula.py
+++ PromptFormula.py
@@ -8,7 +8,7 @@ topico = st.text_input('Insira o TEMA para a geração do texto (exemplo - Liter
 st.write('Agora escolha faça as escolhas para o tipo de utilização que quer fazer ')
 tom = st.selectbox('Escolha o Tom:', ['Formal', 'Casual', 'Informativo', 'Persuasivo', 'Técnico'])
 publico = st.selectbox('Escolha o Público:', ['Estudantes', 'Profissionais', 'Investidores', 'Público Geral','Crianças'])
-formato = st.selectbox('Escolha o Formato:', ['Esboço', 'Pontos Principais', 'Diálogo', 'Parágrafos', 'Plano de Aula', 'Argumentação de Prós e Contras', 'Lista de Palavras-Chave', 'Sugestões de Título para YouTube'])
+formato = st.selectbox('Escolha o Formato:', ['Esboço', 'Pontos Principais', 'Diálogo', 'Parágrafos', 'Plano de Aula', 'Argumentação de Prós e Contras', 'Lista de Palavras-Chave', '5 Sugestões de Título para YouTube'])
 atuar_como = st.selectbox('Atuar como:', ['Especialista', 'Crítico', 'Entusiasta', 'Observador'])
 objetivo = st.selectbox('Escolha o Objetivo:', ['Informar', 'Persuadir', 'Entreter', 'Educar', 'Inspirar'])
 
