b45dfcc12847c962ca8ad5e5e04cf6b57e17d6c7
mcszen1 <mcszen@gmail.com>
2023-11-16 09:36:15 -0300

    Update PromptFormula.py

--- PromptFormula.py
+++ PromptFormula.py
@@ -9,5 +9,5 @@ atuar_como = st.selectbox('Atuar como:', ['Especialista', 'Crítico', 'Entusiast
 objetivo = st.selectbox('Escolha o Objetivo:', ['Informar', 'Persuadir', 'Entreter', 'Educar', 'Inspirar'])
 
 if st.button('Gerar Prompt'):
-    prompt_gerado = f"{topico} - {tom}, {publico}, {formato}, {atuar_como}, {objetivo}"
+    prompt_gerado = f"Você é um {atuar_como} com grande conhecimento sobre {topico}. Quero que gere um texto do tipo {formato}, com tom {tom} e focado num público formado por {publico}. O principal objetivo do texto será {objetivo}."
     st.write('Prompt Gerado:', prompt_gerado)
